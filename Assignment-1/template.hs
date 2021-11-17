f430d8126a2bd8efa2431878c18b94ee7896337b
Owl-Boy <76904491+Owl-Boy@users.noreply.github.com>
2021-11-17 13:09:37 +0530

    Added a folder for Assignment 1
    Added a folder to add programs for Assignment1, contains the questions and the template

---
+++ Assignment-1/template.hs
@@ -0,0 +1,20 @@
+-- Problem 1
+nextSquare :: Integer -> Integer
+
+-- Problem 2
+previousCube :: Integer -> Integer
+
+-- Problem 3
+nextPalin :: Integer -> Integer
+
+-- Problem 4
+primesIn :: Integer -> Integer -> [Integer]
+
+-- Problem 5
+isPrime :: Integer -> Bool
+
+-- Problem 6
+decToBin :: Integer -> Integer
+
+-- Problem 7
+binToDec :: Integer -> Integer
